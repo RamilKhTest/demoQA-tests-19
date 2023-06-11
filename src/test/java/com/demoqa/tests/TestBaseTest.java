a4e75d797f5a966aaf1a1f877e8d34d78c83b9a5
Ramil Khakimov <rkhakimov@yandex.ru>
2023-06-11 21:07:51 +0300

    Add remote

--- src/test/java/com/demoqa/tests/TestBaseTest.java
+++ src/test/java/com/demoqa/tests/TestBaseTest.java
@@ -15,5 +15,6 @@ public class TestBaseTest {
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.browserSize = "1920x1080";
         Configuration. pageLoadStrategy="eager";
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
     }
 }
